5beca29cf1415eef880cb8eee6ba5d40a8c33f08
psryz <psryz@LAPTOP-8F1BP8FG>
2026-09-23 00:17:38 +0300

    Добавьте файлы проекта.

---
+++ Class1.cs
@@ -0,0 +1,13 @@
+﻿namespace CashReaper;
+
+public class Class1 : ChartStrategy
+{
+    public Class1()
+    {
+        Name = "CashReaper";
+    }
+
+    protected override void OnCalculate(int bar, decimal value)
+    {
+    }
+}
